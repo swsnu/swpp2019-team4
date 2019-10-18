a18c4ed40267b9b2f9f71c686af20c162caed437
cube-c <cubecube1000@gmail.com>
2019-10-18 14:25:34 +0900

    add signin api

--- backend/assaapp/tests.py
+++ backend/assaapp/tests.py
@@ -1,3 +1,51 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from assaapp.models import User
+import json
+    
+class AssaTestCase(TestCase):
+    def setUp(self):
+        self.client = Client(enforce_csrf_checks=True)
+        user = User.objects.create_user(email='cubec', password='cubec', username='Jung Jaeyun')
 
-# Create your tests here.
+    def get(self, *args, **kwargs):
+        return self.client.get(*args, **kwargs)
+    
+    def post(self, *args, **kwargs):
+        response = self.client.get('/api/token/')
+        csrftoken = response.cookies['csrftoken'].value
+        return self.client.post(*args, **kwargs, HTTP_X_CSRFTOKEN=csrftoken)
+    
+    def post_nocsrf(self, *args, **kwargs):
+        return self.client.post(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        response = self.client.get('/api/token/')
+        csrftoken = response.cookies['csrftoken'].value
+        return self.client.delete(*args, **kwargs, HTTP_X_CSRFTOKEN=csrftoken)
+    
+    def put(self, *args, **kwargs):
+        response = self.client.get('/api/token/')
+        csrftoken = response.cookies['csrftoken'].value
+        return self.client.put(*args, **kwargs, HTTP_X_CSRFTOKEN=csrftoken)
+
+    def test_csrf(self):
+        response = self.post('/api/token/')
+        self.assertEqual(response.status_code, 405)
+        response = self.post_nocsrf('/api/signin/', json.dumps({'email': 'cubec', 'password': 'cubec'}),
+            content_type='application/json')
+        self.assertEqual(response.status_code, 403)
+    
+    def test_post_signin(self):
+        response = self.post('/api/signin/', json.dumps({'email': 'cubec', 'Password': 'cubec2'}),
+            content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        response = self.post('/api/signin/', json.dumps({'email': 'cubec', 'password': 'cubec2'}),
+            content_type='application/json')
+        self.assertEqual(response.status_code, 401)
+        response = self.post('/api/signin/', json.dumps({'email': 'cubec', 'password': 'cubec'}),
+            content_type='application/json')
+        self.assertEqual(response.status_code, 204)
+
+    def test_get_signin(self):
+        response = self.get('/api/signin/')
+        self.assertEqual(response.status_code, 405)
