15d031cc9e3c5da251f5e5167973ddae54dd4d81
1207koo <1207koo@naver.com>
2019-11-01 01:43:17 +0900

    edited testing data

--- frontend/src/components/TimeTableView/TimeTableView.test.js
+++ frontend/src/components/TimeTableView/TimeTableView.test.js
@@ -4,20 +4,12 @@ import React from 'react';
 describe('<TimeTableView />', () => {
     it('TimeTableView render test', () => {
         var courses=[
-            {"week_day": 0, "start_time": 660, "end_time": 750, "course_name": "DS", "color": "#2BC366"},
-            {"week_day": 2, "start_time": 660, "end_time": 750, "course_name": "DS", "color": "#2BC366"},
-            {"week_day": 4, "start_time": 840, "end_time": 960, "course_name": "DS", "color": "#2BC366"},
-            {"week_day": 0, "start_time": 840, "end_time": 930, "course_name": "전기전자회로", "color": "#7FFF00"},
-            {"week_day": 2, "start_time": 840, "end_time": 930, "course_name": "전기전자회로", "color": "#7FFF00"},
-            {"week_day": 0, "start_time": 930, "end_time": 1020, "course_name": "컴퓨터구조", "color": "#FFD700"},
-            {"week_day": 2, "start_time": 930, "end_time": 1020, "course_name": "컴퓨터구조", "color": "#FFD700"},
-            {"week_day": 1, "start_time": 930, "end_time": 990, "course_name": "프로그래밍의원리", "color": "#664BC3"},
-            {"week_day": 3, "start_time": 930, "end_time": 990, "course_name": "프로그래밍의원리", "color": "#664BC3"},
-            {"week_day": 1, "start_time": 1110, "end_time": 1230, "course_name": "프로그래밍의원리", "color": "#664BC3"},
-            {"week_day": 2, "start_time": 780, "end_time": 840, "course_name": "컴공세미나", "color": "#00C3F2"},
-            {"week_day": 0, "start_time": 1020, "end_time": 1110, "course_name": "swpp", "color": "#FF2312"},
-            {"week_day": 2, "start_time": 1020, "end_time": 1110, "course_name": "swpp", "color": "#FF2312"},
-            {"week_day": 3, "start_time": 1110, "end_time": 1230, "course_name": "swpp", "color": "#FF2312"},
+            {"week_day": 0, "start_time": 660, "end_time": 750, "course_name": "DS", "color": "#FFFFFF"},
+            {"week_day": 0, "start_time": 1020, "end_time": 1110, "course_name": "swpp", "color": "#000000"},
+            {"week_day": 2, "start_time": 660, "end_time": 750, "course_name": "DS", "color": "#FFFFFF"},
+            {"week_day": 2, "start_time": 1020, "end_time": 1110, "course_name": "swpp", "color": "#000000"},
+            {"week_day": 3, "start_time": 1110, "end_time": 1230, "course_name": "swpp", "color": "#000000"},
+            {"week_day": 4, "start_time": 840, "end_time": 960, "course_name": "DS", "color": "#FFFFFF"},
         ]
         const component = mount(<TimeTableView courses={courses}/>);
         const datastructure=component.find('#timetable');
