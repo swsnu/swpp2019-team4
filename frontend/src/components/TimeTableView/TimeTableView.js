1519cd0c5d2b0e33d89dbfae6a0ed1e61c7c0a41
1207koo <1207koo@naver.com>
2019-10-31 19:54:25 +0900

    var -> let

--- frontend/src/components/TimeTableView/TimeTableView.js
+++ frontend/src/components/TimeTableView/TimeTableView.js
@@ -3,8 +3,8 @@ import { connect } from 'react-redux';
 import { Redirect, NavLink } from 'react-router-dom';
 import * as actionCreators from '../../store/actions/index';
 /*
- * INPUT: props.timetable parsed as [{"day_index": 0, "start_time_index": 0, "end_time_index": 3, "course_name": "NAME OF EACH COURSE"}, {}, ...]
- * day_index is 0~5 integer. Monday is 0, Tuesday is 1, ... Saturday is 5
+ * INPUT: props.timetable parsed as [{"week_day": 0, "start_time_index": 0, "end_time_index": 3, "course_name": "NAME OF EACH COURSE"}, {}, ...]
+ * week_day is 0~5 integer. Monday is 0, Tuesday is 1, ... Saturday is 5
  * start_time_index, end_time_index are 16~41 integer. 8:00 is 16, 8:30 is 17, ... 20:30 is 41.
  * If the course time is 8:30~9:30, start_time_index is 17, end_time_index is 19
 */
@@ -37,18 +37,18 @@ const TimeTableView=(props)=>{
     ["19:00"," "," "," "," "," "," "],
     ["20:00"," "," "," "," "," "," "],
     ["20:00"," "," "," "," "," "," "],];
-    for(var i=0; i<props.courses.length; i++){
-        var day=props.courses[i]["day_index"]+1;
-        for(var j=props.courses[i]["start_time_index"]-15;j<props.courses[i]["end_time_index"]-15;j++){
+    for(let i=0; i<props.courses.length; i++){
+        var day=props.courses[i]["week_day"]+1;
+        for(let j=props.courses[i]["start_time_index"]-15;j<props.courses[i]["end_time_index"]-15;j++){
             tablestring[j][day]=props.courses[i]["course_name"];
         }
     }
     var tablehtml=[];
     var heightunit=20;
     var widthunit=100;
-    for(var i=0;i<tablestring.length;i++){
+    for(let i=0;i<tablestring.length;i++){
         var tablehtml_ith=[];
-        for(var j=0;j<tablestring[i].length;j++){
+        for(let j=0;j<tablestring[i].length;j++){
             if(i>1&&tablestring[i][j]==tablestring[i-1][j])continue;
             var sametext_length=1;
             for(var k=i+1;k<tablestring.length;k++){
@@ -61,7 +61,6 @@ const TimeTableView=(props)=>{
         tablehtml.push(<tr key={i}>{tablehtml_ith}</tr>);
     }
     //tablestring.map(row=><tr key={row[0]}><th width="100">{row[0]}</th><td width="100">{row[1]}</td><td width="100">{row[2]}</td><td width="100">{row[3]}</td><td width="100">{row[4]}</td><td width="100">{row[5]}</td><td width="100">{row[6]}</td></tr>)
-    console.log(tablehtml);
     return (
         <div className='Timetableview'>
             <table id="timetable" border="1" bordercolor="black" width ="800" height="500">
