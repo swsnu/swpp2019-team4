5d19e46d8fbf6b7265684c3b0f557fd6e13d9a8d
vionicbest <vionicbest@snu.ac.kr>
2019-11-01 11:26:42 +0900

    add views

--- backend/assaapp/views.py
+++ backend/assaapp/views.py
@@ -12,6 +12,74 @@ from json import JSONDecodeError
 from .tokens import account_activation_token
 import json
 
+def make_course(req_data, course):
+    course_semester = req_data['semester']
+    course_classification = req_data['classification']
+    course_college = req_data['college']
+    course_department = req_data['department']
+    course_degree_program = req_data['degree_program']
+    course_academic_year = req_data['academic_year']
+    course_course_number = req_data['course_number']
+    course_lecture_number = req_data['lecture_number']
+    course_title = req_data['title']
+    course_subtitle = req_data['subtitle']
+    course_credit = req_data['credit']
+    course_lecture_credit = req_data['lecture_credit']
+    course_lab_credit = req_data['lab_credit']
+    course_lecture_type = req_data['lecture_type']
+    course_location = req_data['location']
+    course_professor = req_data['professor']
+    course_quota = req_data['quota']
+    course_remark = req_data['remark']
+    course_language = req_data['language']
+    course_status = req_data['status']
+    if course == None:
+        course = Course(
+            semester=course_semester,
+            classification=course_classification,
+            college=course_college,
+            department=course_department,
+            degree_program=course_degree_program,
+            academic_year=course_academic_year,
+            course_number=course_course_number,
+            lecture_number=course_lecture_number,
+            title=course_title,
+            subtitle=course_subtitle,
+            credit=course_credit,
+            lecture_credit=course_lecture_credit,
+            lab_credit=course_lab_credit,
+            lecture_type=course_lecture_type,
+            location=course_location,
+            professor=course_professor,
+            quota=course_quota,
+            remark=course_remark,
+            language=course_language,
+            status=course_status
+        )
+        return course
+    else:
+        course.semester=course_semester
+        course.classification=course_classification
+        course.college=course_college
+        course.department=course_department
+        course.degree_program=course_degree_program
+        course.academic_year=course_academic_year
+        course.course_number=course_course_number
+        course.lecture_number=course_lecture_number
+        course.title=course_title
+        course.subtitle=course_subtitle
+        course.credit=course_credit
+        course.lecture_credit=course_lecture_credit
+        course.lab_credit=course_lab_credit
+        course.lecture_type=course_lecture_type
+        course.location=course_location
+        course.professor=course_professor
+        course.quota=course_quota
+        course.remark=course_remark
+        course.language=course_language
+        course.status=course_status
+        return course
+
 def signup(request):
     if request.method == 'POST':
         try:
@@ -162,8 +230,25 @@ def timetable_id_course(request, timetable_id):
             timetable = Timetable.objects.get(pk=timetable_id)
             courses = [course for course in timetable.courses.all().values()]
             return JsonResponse(courses, status=200, safe=False)
+        if request.method == 'POST':
+            try:
+                body = request.body.decode()
+                course_id = json.loads(body)['course_id']
+                timetable_list = [timetable for timetable in Timetable.objects.all().values() if timetable['id'] == timetable_id]
+                if len(timetable_list) == 0:
+                    return HttpResponseNotFound()
+                timetable = Timetable.objects.get(pk=timetable_id)
+                course_list = [course for course in Course.objects.all().values() if course['id'] == course_id]
+                if len(course_list) == 0:
+                    return HttpResponseNotFound()
+                course = Course.objects.get(pk=course_id)
+                timetable.courses.add(course)
+                timetable.save()
+                return HttpResponse(status = 200)
+            except (KeyError, JSONDecodeError) as e:
+                return HttpResponseBadRequest()
         else:
-            return HttpResponseNotAllowed(['GET'])
+            return HttpResponseNotAllowed(['GET'],['POST'])
     else:
         return HttpResponse(status=401)
 
@@ -175,49 +260,7 @@ def course(request):
         if request.method == 'POST':
             try:
                 req_data = json.loads(request.body.decode())
-                course_semester = req_data['semester']
-                course_classification = req_data['classification']
-                course_college = req_data['college']
-                course_department = req_data['department']
-                course_degree_program = req_data['degree_program']
-                course_academic_year = req_data['academic_year']
-                course_course_number = req_data['course_number']
-                course_lecture_number = req_data['lecture_number']
-                course_title = req_data['title']
-                course_subtitle = req_data['subtitle']
-                course_credit = req_data['credit']
-                course_lecture_credit = req_data['lecture_credit']
-                course_lab_credit = req_data['lab_credit']
-                course_lecture_type = req_data['lecture_type']
-                course_location = req_data['location']
-                course_professor = req_data['professor']
-                course_quota = req_data['quota']
-                course_remark = req_data['remark']
-                course_language = req_data['language']
-                course_status = req_data['status']
-
-                course = Course(
-                    semester=course_semester,
-                    classification=course_classification,
-                    college=course_college,
-                    department=course_department,
-                    degree_program=course_degree_program,
-                    academic_year=course_academic_year,
-                    course_number=course_course_number,
-                    lecture_number=course_lecture_number,
-                    title=course_title,
-                    subtitle=course_subtitle,
-                    credit=course_credit,
-                    lecture_credit=course_lecture_credit,
-                    lab_credit=course_lab_credit,
-                    lecture_type=course_lecture_type,
-                    location=course_location,
-                    professor=course_professor,
-                    quota=course_quota,
-                    remark=course_remark,
-                    language=course_language,
-                    status=course_status
-                )
+                course = make_course(req_data, None)
                 course.save()
                 return HttpResponse(status=201)
             except (KeyError, JSONDecodeError) as e:
@@ -237,50 +280,11 @@ def course_id(request, course_id):
             return JsonResponse(model_to_dict(course), status=200)
         if request.method == 'PUT':
             req_data = json.loads(request.body.decode())
-            course_semester = req_data['semester']
-            course_classification = req_data['classification']
-            course_college = req_data['college']
-            course_department = req_data['department']
-            course_degree_program = req_data['degree_program']
-            course_academic_year = req_data['academic_year']
-            course_course_number = req_data['course_number']
-            course_lecture_number = req_data['lecture_number']
-            course_title = req_data['title']
-            course_subtitle = req_data['subtitle']
-            course_credit = req_data['credit']
-            course_lecture_credit = req_data['lecture_credit']
-            course_lab_credit = req_data['lab_credit']
-            course_lecture_type = req_data['lecture_type']
-            course_location = req_data['location']
-            course_professor = req_data['professor']
-            course_quota = req_data['quota']
-            course_remark = req_data['remark']
-            course_language = req_data['language']
-            course_status = req_data['status']
             course_list = [course for course in Course.objects.all().values() if course['id'] == course_id]
             if len(course_list) == 0:
                 return HttpResponseNotFound()
             course = Course.objects.get(pk=course_id)
-            course.semester=course_semester
-            course.classification=course_classification
-            course.college=course_college
-            course.department=course_department
-            course.degree_program=course_degree_program
-            course.academic_year=course_academic_year
-            course.course_number=course_course_number
-            course.lecture_number=course_lecture_number
-            course.title=course_title
-            course.subtitle=course_subtitle
-            course.credit=course_credit
-            course.lecture_credit=course_lecture_credit
-            course.lab_credit=course_lab_credit
-            course.lecture_type=course_lecture_type
-            course.location=course_location
-            course.professor=course_professor
-            course.quota=course_quota
-            course.remark=course_remark
-            course.language=course_language
-            course.status=course_status
+            course = make_course(req_data, course)
             course.save()
             return JsonResponse(model_to_dict(course),status=200)
         if request.method == 'DELETE':
