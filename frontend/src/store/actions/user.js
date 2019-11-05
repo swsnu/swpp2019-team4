f51b0a213775d3320ddc89d1f079c578c537e49e
khsoo01 <khsoo01@snu.ac.kr>
2019-11-05 19:56:34 +0900

    alert usage in login resolved

--- frontend/src/store/actions/user.js
+++ frontend/src/store/actions/user.js
@@ -7,10 +7,7 @@ export const postSignup = (email, password, username, department, grade) => () =
 
 export const postSignin = (email, password) => (dispatch) => axios.post('/api/signin/', { email, password })
   .then(() => dispatch({ type: actionTypes.GET_AUTH, is_authenticated: true }))
-  .catch(() => {
-    dispatch({ type: actionTypes.GET_AUTH, is_authenticated: false });
-    alert('이메일 또는 비밀번호가 잘못되었습니다.');
-  });
+  .catch(() => dispatch({ type: actionTypes.GET_AUTH, is_authenticated: false }));
 
 export const getUser = () => (dispatch) => axios.get('/api/user/')
   .then((res) => dispatch({ type: actionTypes.GET_USER, user: res.data }))
