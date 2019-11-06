e7071d47148ea651dd25315d68f6408a31a419c8
1207koo <1207koo@naver.com>
2019-11-06 10:57:56 +0900

    lintify

--- frontend/src/components/TimetableView/TimetableView.test.js
+++ frontend/src/components/TimetableView/TimetableView.test.js
@@ -1,7 +1,6 @@
 import { mount } from 'enzyme';
 import React from 'react';
 import TimetableView from './TimetableView';
-import TimetableGenerator from '../TimetableGenerator/TimetableGenerator';
 
 describe('<TimetableView />', () => {
   it('TimetableView render test', () => {
@@ -16,15 +15,6 @@ describe('<TimetableView />', () => {
         lecture_number: '001',
       },
       {
-        week_day: 0,
-        start_time: 1020,
-        end_time: 1110,
-        course_name: '소프트웨어 개발의 원리와 실습',
-        color: '#FF2312',
-        course_number: 'M1522.002400',
-        lecture_number: '001',
-      },
-      {
         week_day: 2,
         start_time: 660,
         end_time: 750,
@@ -34,102 +24,12 @@ describe('<TimetableView />', () => {
         lecture_number: '001',
       },
       {
-        week_day: 2,
-        start_time: 1020,
-        end_time: 1110,
-        course_name: '소프트웨어 개발의 원리와 실습',
-        color: '#FF2312',
-        course_number: 'M1522.002400',
-        lecture_number: '001',
-      },
-      {
         week_day: 4,
         start_time: 840,
         end_time: 960,
         course_name: '자료구조',
         color: '#2BC366',
         course_number: 'M1522.000900',
-        lecture_number: '001',
-      },
-      {
-        week_day: 0,
-        start_time: 840,
-        end_time: 930,
-        course_name: '전기전자회로',
-        color: '#7FFF00',
-        course_number: '4190.206A',
-        lecture_number: '001',
-      },
-      {
-        week_day: 2,
-        start_time: 840,
-        end_time: 930,
-        course_name: '전기전자회로',
-        color: '#7FFF00',
-        course_number: '4190.206A',
-        lecture_number: '001',
-      },
-      {
-        week_day: 0,
-        start_time: 930,
-        end_time: 1020,
-        course_name: '컴퓨터구조',
-        color: '#FFD700',
-        course_number: '4190.308',
-        lecture_number: '002',
-      },
-      {
-        week_day: 2,
-        start_time: 930,
-        end_time: 1020,
-        course_name: '컴퓨터구조',
-        color: '#FFD700',
-        course_number: '4190.308',
-        lecture_number: '002',
-      },
-      {
-        week_day: 1,
-        start_time: 930,
-        end_time: 990,
-        course_name: '프로그래밍의원리',
-        color: '#866BC3',
-        course_number: '4190.210',
-        lecture_number: '001',
-      },
-      {
-        week_day: 3,
-        start_time: 930,
-        end_time: 990,
-        course_name: '프로그래밍의원리',
-        color: '#866BC3',
-        course_number: '4190.210',
-        lecture_number: '001',
-      },
-      {
-        week_day: 1,
-        start_time: 1110,
-        end_time: 1230,
-        course_name: '프로그래밍의원리',
-        color: '#866BC3',
-        course_number: '4190.210',
-        lecture_number: '001',
-      },
-      {
-        week_day: 2,
-        start_time: 780,
-        end_time: 840,
-        course_name: '컴퓨터공학세미나',
-        color: '#FF00FF',
-        course_number: '4190.209',
-        lecture_number: '001',
-      },
-      {
-        week_day: 3,
-        start_time: 1110,
-        end_time: 1230,
-        course_name: '소프트웨어 개발의 원리와 실습',
-        color: '#00FFFF',
-        course_number: 'M1522.002400',
         lecture_number: '001',
       },
     ];
@@ -152,8 +52,8 @@ describe('<TimetableView />', () => {
       />,
     );
     const datastructureFT = componentFT.find('.square');
-    expect(datastructureFT.length).toBe(6);
+    expect(datastructureFT.length).toBe(3);
     const datastructureTF = componentTF.find('.square');
-    expect(datastructureTF.length).toBe(6);
+    expect(datastructureTF.length).toBe(3);
   });
 });
--- frontend/src/containers/TimetableRecommend/TimetableRecommend.test.js
+++ frontend/src/containers/TimetableRecommend/TimetableRecommend.test.js
@@ -6,7 +6,6 @@ import { ConnectedRouter } from 'connected-react-router';
 import { createBrowserHistory } from 'history';
 import { getMockStore } from '../../test-utils/mocks';
 import TimetableRecommend from './TimetableRecommend';
-import TimetableGenerator from '../../components/TimetableGenerator/TimetableGenerator';
 
 const stubState = {
   user: { is_authenticated: true },
@@ -14,7 +13,136 @@ const stubState = {
 
 function timetableManagement(state) {
   const mockStore = getMockStore(state);
-  const recommendlist = [TimetableGenerator(0)];
+  const recommendlist = [
+    [
+      {
+        week_day: 0,
+        start_time: 660,
+        end_time: 750,
+        course_name: '자료구조',
+        color: '#2BC366',
+        course_number: 'M1522.000900',
+        lecture_number: '001',
+      },
+      {
+        week_day: 0,
+        start_time: 1020,
+        end_time: 1110,
+        course_name: '소프트웨어 개발의 원리와 실습',
+        color: '#FF2312',
+        course_number: 'M1522.002400',
+        lecture_number: '001',
+      },
+      {
+        week_day: 2,
+        start_time: 660,
+        end_time: 750,
+        course_name: '자료구조',
+        color: '#2BC366',
+        course_number: 'M1522.000900',
+        lecture_number: '001',
+      },
+      {
+        week_day: 2,
+        start_time: 1020,
+        end_time: 1110,
+        course_name: '소프트웨어 개발의 원리와 실습',
+        color: '#FF2312',
+        course_number: 'M1522.002400',
+        lecture_number: '001',
+      },
+      {
+        week_day: 4,
+        start_time: 840,
+        end_time: 960,
+        course_name: '자료구조',
+        color: '#2BC366',
+        course_number: 'M1522.000900',
+        lecture_number: '001',
+      },
+      {
+        week_day: 0,
+        start_time: 840,
+        end_time: 930,
+        course_name: '전기전자회로',
+        color: '#7FFF00',
+        course_number: '4190.206A',
+        lecture_number: '001',
+      },
+      {
+        week_day: 2,
+        start_time: 840,
+        end_time: 930,
+        course_name: '전기전자회로',
+        color: '#7FFF00',
+        course_number: '4190.206A',
+        lecture_number: '001',
+      },
+      {
+        week_day: 0,
+        start_time: 930,
+        end_time: 1020,
+        course_name: '컴퓨터구조',
+        color: '#FFD700',
+        course_number: '4190.308',
+        lecture_number: '002',
+      },
+      {
+        week_day: 2,
+        start_time: 930,
+        end_time: 1020,
+        course_name: '컴퓨터구조',
+        color: '#FFD700',
+        course_number: '4190.308',
+        lecture_number: '002',
+      },
+      {
+        week_day: 1,
+        start_time: 930,
+        end_time: 990,
+        course_name: '프로그래밍의원리',
+        color: '#866BC3',
+        course_number: '4190.210',
+        lecture_number: '001',
+      },
+      {
+        week_day: 3,
+        start_time: 930,
+        end_time: 990,
+        course_name: '프로그래밍의원리',
+        color: '#866BC3',
+        course_number: '4190.210',
+        lecture_number: '001',
+      },
+      {
+        week_day: 1,
+        start_time: 1110,
+        end_time: 1230,
+        course_name: '프로그래밍의원리',
+        color: '#866BC3',
+        course_number: '4190.210',
+        lecture_number: '001',
+      },
+      {
+        week_day: 2,
+        start_time: 780,
+        end_time: 840,
+        course_name: '컴퓨터공학세미나',
+        color: '#FF00FF',
+        course_number: '4190.209',
+        lecture_number: '001',
+      },
+      {
+        week_day: 3,
+        start_time: 1110,
+        end_time: 1230,
+        course_name: '소프트웨어 개발의 원리와 실습',
+        color: '#00FFFF',
+        course_number: 'M1522.002400',
+        lecture_number: '001',
+      },
+    ],
+  ];
   return (
     <Provider store={mockStore}>
       <ConnectedRouter history={createBrowserHistory()}>
