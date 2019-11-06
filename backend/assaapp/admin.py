8fbeffed8bf531b1b4b410a0cc550c1ac4f8498c
vionicbest <vionicbest@snu.ac.kr>
2019-11-06 19:58:13 +0900

    add show timetables and courses

--- backend/assaapp/admin.py
+++ backend/assaapp/admin.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
-from assaapp.models import User
+from assaapp.models import User, Timetable, Course, CourseTime
 
 class UserCreationForm(forms.ModelForm):
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
@@ -62,4 +62,7 @@ class UserAdmin(BaseUserAdmin):
 
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Timetable)
+admin.site.register(Course)
+admin.site.register(CourseTime)
 admin.site.unregister(Group)
