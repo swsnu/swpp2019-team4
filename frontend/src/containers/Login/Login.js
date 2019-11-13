81517d936124781e007c07f3817fb1006fc740d8
cube-c <cubecube1000@gmail.com>
2019-11-14 06:12:45 +0900

    Making login page

--- frontend/src/containers/Login/Login.js
+++ frontend/src/containers/Login/Login.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { NavLink, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
 import * as actionCreators from '../../store/actions/index';
 import './Login.css';
 
@@ -42,37 +39,39 @@ class Login extends Component {
     const loginNotice = (this.state.login_failed ? '이메일 또는 비밀번호가 잘못되었습니다.' : '');
     return (
       <div className="Login">
-        <h1><b> ASSA </b></h1>
-        <br />
-        <Form>
-          <Form.Group as={Row}>
-            <Form.Control
-              type="text"
-              id="email-input"
-              value={this.state.email}
-              placeholder="Email"
-              onChange={(event) => this.setState({ email: event.target.value })}
-            />
-            <Form.Control
-              type="password"
-              id="pw-input"
-              value={this.state.pasword}
-              placeholder="Password"
-              onChange={(event) => this.setState({ password: event.target.value })}
-            />
-          </Form.Group>
-          <Button type="button" id="login-button" onClick={() => this.handleLogin()}>로그인</Button>
-          <Button
-            variant="outline-primary"
-            type="button"
-            id="to-signup-button"
-            onClick={() => this.goToSignup()}
-          >
-회원가입
-          </Button>
-          <p id="login-notice">{loginNotice}</p>
-        </Form>
-        <br />
+        <div className="row">
+        <div className="col d-flex flex-column ml-5 justify-content-center">
+          <h1 className="text-left mb-0"><b><span className="first-letter"> a</span>utomatic </b></h1>
+          <h1 className="text-left mb-0"><b><span className="first-letter"> s</span>cheduling& </b></h1>
+          <h1 className="text-left mb-0"><b><span className="first-letter"> s</span>haring </b></h1>
+          <h1 className="text-left mb-0"><b><span className="first-letter"> a</span>pplication </b></h1>
+        </div>
+        <div className="col d-flex flex-column justify-content-center">
+          <form>
+            <div className="form-group m-2">
+              <input className={"form-control "}
+                type="text"
+                id="email-input"
+                value={this.state.email}
+                placeholder="Email"
+                onChange={(event) => this.setState({ email: event.target.value })}
+              />
+              </div>
+            <div className="form-group m-2">
+              <input className={"form-control " + (this.state.login_failed ? "is-invalid" : "")}
+                type="password"
+                id="pw-input"
+                value={this.state.pasword}
+                placeholder="Password"
+                onChange={(event) => this.setState({ password: event.target.value })}
+              />
+             <div className="small invalid-feedback text-left" id="login-notice">{loginNotice}</div>
+             </div>
+          </form>
+            <button className="btn btn-dark m-2" type="button" id="login-button" onClick={() => this.handleLogin()}>로그인</button>
+            <div><NavLink className="text-body" to="signup"><small><b>회원가입</b></small></NavLink>
+            </div>
+        </div></div>
       </div>
     );
   }
