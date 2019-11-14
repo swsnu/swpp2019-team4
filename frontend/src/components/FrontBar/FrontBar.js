ea70190ff10cfd4ce6a1d9ad6a7c526002416d0a
cube-c <cubecube1000@gmail.com>
2019-11-14 17:49:35 +0900

    Remove unnecessary signup test and add logo redirection

--- frontend/src/components/FrontBar/FrontBar.js
+++ frontend/src/components/FrontBar/FrontBar.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './FrontBar.css';
 
 const FrontBar = () => (
   <div className="FrontBar pt-1">
-    {/*<h2 className="oi oi-chevron-left text-dark m-0" id="prev-logo" />*/}
-    <img className="" src="/logo192.png" alt="logo" id="assa-logo" />
+    {/* <h2 className="oi oi-chevron-left text-dark m-0" id="prev-logo" /> */}
+    <NavLink to="/login">
+      <img className="" src="/logo192.png" alt="logo" id="assa-logo" />
+    </NavLink>
   </div>
 );
 
--- frontend/src/containers/Signup/Signup.js
+++ frontend/src/containers/Signup/Signup.js
@@ -28,8 +28,8 @@ class Signup extends Component {
       email_notice: '',
       password_notice: '',
       password_confirm_notice: '',
-      username_notice:'',
-      grade_notice:'',
+      username_notice: '',
+      grade_notice: '',
       department_notice: '',
 
       is_waiting: false,
@@ -40,16 +40,16 @@ class Signup extends Component {
     this.props.onGetUser();
   }
 
-  handleSignup(email, password, password_confirm, username, department, grade) {
+  handleSignup(email, password, passwordConfirm, username, department, grade) {
     const regexValid = /^[^@\s]+@[^.@\s]+[.][^@\s]+$/.exec(email);
     const emailValid = (regexValid !== null);
     const passwordValid = (password.length >= 8 && password.length <= 32);
-    const passwordConfirmValid = (password === password_confirm);
+    const passwordConfirmValid = (password === passwordConfirm);
     const usernameValid = (username.length >= 1 && username.length <= 16);
     const departmentValid = (department === '컴퓨터공학부');
     const gradeValid = (1 * grade >= 1 && 1 * grade <= 99);
-    const sendValid = emailValid && passwordValid && passwordConfirmValid && usernameValid &&
-     departmentValid && gradeValid;
+    const sendValid = emailValid && passwordValid && passwordConfirmValid && usernameValid
+     && departmentValid && gradeValid;
 
     const emailNotice = (emailValid ? '' : '이메일 형식이 올바르지 않습니다.');
     const passwordNotice = (passwordValid ? '' : '비밀번호는 8자 이상 32자 이하로 구성되어야 합니다.');
@@ -58,19 +58,32 @@ class Signup extends Component {
     const gradeNotice = (gradeValid ? '' : '학년은 1 이상 99 이하의 정수여야 합니다.');
     const departmentNotice = (departmentValid ? '' : '존재하는 학과를 입력해야 합니다.');
 
-    this.setState((prevState) => ({ ...prevState, email_valid: emailValid, password_valid: passwordValid,
-      password_confirm_valid: passwordConfirmValid, username_valid: usernameValid, department_valid: departmentValid,
-      grade_valid: gradeValid, email_notice: emailNotice, password_notice: passwordNotice,
-      password_confirm_notice: passwordConfirmNotice, username_notice: usernameNotice, department_notice: departmentNotice,
-      grade_notice: gradeNotice}));
+    this.setState((prevState) => ({
+      ...prevState,
+      email_valid: emailValid,
+      password_valid: passwordValid,
+      password_confirm_valid: passwordConfirmValid,
+      username_valid: usernameValid,
+      department_valid: departmentValid,
+      grade_valid: gradeValid,
+      email_notice: emailNotice,
+      password_notice: passwordNotice,
+      password_confirm_notice: passwordConfirmNotice,
+      username_notice: usernameNotice,
+      department_notice: departmentNotice,
+      grade_notice: gradeNotice,
+    }));
     if (sendValid) {
-      this.setState((prevState) => ({ ...prevState, is_waiting: true}));
+      this.setState((prevState) => ({ ...prevState, is_waiting: true }));
       this.props.onPostSignup(email, password, username, department, grade)
         .then(() => {
           this.props.history.push('/login');
         })
-        .catch(() => this.setState((prevState) => ({ ...prevState, email_valid: false, is_waiting: false,
-          email_notice: '이미 존재하는 이메일입니다.'
+        .catch(() => this.setState((prevState) => ({
+          ...prevState,
+          email_valid: false,
+          is_waiting: false,
+          email_notice: '이미 존재하는 이메일입니다.',
         })));
     }
   }
@@ -148,28 +161,29 @@ class Signup extends Component {
               <div className="small text-left violation-notice feedback">{this.state.grade_notice}</div>
             </div>
             <div className="row m-0">
-            <button
-              className="btn btn-outline-dark col mr-1"
-              type="button"
-              id="to-login-button"
-              onClick={() => this.goToLogin()}
-            >
+              <button
+                className="btn btn-outline-dark col mr-1"
+                type="button"
+                id="to-login-button"
+                onClick={() => this.goToLogin()}
+              >
 뒤로가기
-            </button>
-            <button
-              className="btn btn-dark col ml-1"
-              type="button"
-              id="confirm-signup-button"
-              disabled={this.state.is_waiting}
-              onClick={() => this.handleSignup(this.state.email,
-                this.state.password,
-                this.state.password_confirm,
-                this.state.username,
-                this.state.department,
-                this.state.grade)}
-            >
+              </button>
+              <button
+                className="btn btn-dark col ml-1"
+                type="button"
+                id="confirm-signup-button"
+                disabled={this.state.is_waiting}
+                onClick={() => this.handleSignup(this.state.email,
+                  this.state.password,
+                  this.state.password_confirm,
+                  this.state.username,
+                  this.state.department,
+                  this.state.grade)}
+              >
 가입하기
-            </button></div>
+              </button>
+            </div>
           </form>
         </div>
       </div>
@@ -181,7 +195,7 @@ Signup.propTypes = {
   onGetUser: PropTypes.func.isRequired,
   onPostSignup: PropTypes.func.isRequired,
   history: PropTypes.shape({
-    replace: PropTypes.func.isRequired,
+    push: PropTypes.func.isRequired,
   }).isRequired,
   storedUser: PropTypes.shape({
     is_authenticated: PropTypes.bool,
