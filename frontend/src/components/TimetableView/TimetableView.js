3e8dd669d7d30feaa303c53b1ca82fa2b35594b9
cube-c <cubecube1000@gmail.com>
2019-11-19 03:52:34 +0900

    Remake timetable UI

--- frontend/src/components/TimetableView/TimetableView.js
+++ frontend/src/components/TimetableView/TimetableView.js
@@ -6,11 +6,16 @@ import './TimetableView.css';
  * [{"week_day": 0, "start_time": 660, "end_time": 750, "name": "NAME OF EACH COURSE", "color": "#FFFFFF", "course_number": "COU.NUM", "lecture_number": "001"}, {}, ...]
  * week_day is 0~5 integer. Monday is 0, Tuesday is 1, ... Saturday is 5
  * start_time, end_time are 660~1230 integer. This value is hour*60+minute (8:00~20:30). start_time must be divided by 30
- * color is 6 hexadigit number.
- * height is constant value, and width is max width percent value
+ * height is constant value, and color is 6 hexadigit number.
  * text is true of false. If text if true, text will show. If false, text will not show and can display more smaller
 */
 
+/* temporary color palette from https://clrs.cc
+ * 13 colors (excluding 3 grayscale colors)
+ */
+
+const colors = ['#FF851B', '#FFDC00', '#39CCCC', '#01FF70', '#F012BE', '#FF4136', '#7FDBFF',
+  '#85144B', '#001F3F', '#3D9970', '#B10DC9', '#2ECC40', '#0074D9'];
 
 const TimetableView = (props) => {
   const tableHeaderString = ['', 'M', 'T', 'W', 'T', 'F', 'S'];
@@ -18,7 +23,6 @@ const TimetableView = (props) => {
   const tablehtml = [];
   let tablehtmlIth = [];
   const heightunit = props.height;
-  const widthunit = '14%';
   for (let i = 0; i < 26; i += 1) {
     for (let j = 0; j < 6; j += 1) {
       coursesList[j].push([]);
@@ -27,11 +31,13 @@ const TimetableView = (props) => {
   if (props.courses !== undefined) {
     for (let i = 0; i < props.courses.length; i += 1) {
       for (let j = 0; j < props.courses[i].time.length; j += 1) {
-        coursesList[props.courses[i].time[j].week_day][props.courses[i].time[j].start_time / 30 - 16].push(
+        const segment = props.courses[i].time[j];
+        coursesList[segment.week_day][segment.start_time / 30 - 16].push(
           {
             title: props.text ? props.courses[i].title : '',
-            length: props.courses[i].time[j].end_time - props.courses[i].time[j].start_time,
-            color: props.courses[i].color,
+            length: (parseInt((segment.end_time - segment.start_time) / 30, 10) + 1) * heightunit,
+            // color: props.courses[i].color,
+            color: colors[i % colors.length],
             lecnum: props.courses[i].lecture_number,
             clanum: props.courses[i].course_number,
           },
@@ -40,7 +46,7 @@ const TimetableView = (props) => {
     }
   }
   for (let i = 0; i < 7; i += 1) {
-    tablehtmlIth.push(<th key={i} height={heightunit} width={widthunit}>{props.text ? tableHeaderString[i] : ''}</th>);
+    tablehtmlIth.push(<th key={i} height={heightunit}>{props.text ? tableHeaderString[i] : ''}</th>);
   }
   tablehtml.push(<tr key={-1}>{tablehtmlIth}</tr>);
   for (let i = 0; i < 26; i += 1) {
@@ -48,50 +54,46 @@ const TimetableView = (props) => {
     if (i % 2 === 0) {
       tablehtmlIth.push(
         <td
+          className="text-right pr-2 py-0 small text-black-50"
           key={1000 * i + 1000}
-          height={2 * heightunit}
-          width={widthunit}
           rowSpan={2}
         >
-          {props.text ? `${i / 2 + 8}:00` : ''}
+          {props.text ? `${i / 2 + 8}` : ''}
         </td>,
       );
     }
     for (let j = 0; j < 6; j += 1) {
       if (coursesList[j][i].length === 0) {
-        tablehtmlIth.push(<td key={1000 * i + j + 1001} height={heightunit} width={widthunit} />);
+        tablehtmlIth.push(<td key={1000 * i + j + 1001} height={heightunit} />);
       } else {
         tablehtmlIth.push(
-          <td key={1000 * i + j} height={heightunit} width={widthunit}>
+          <td key={1000 * i + j} height={heightunit}>
             {
               coursesList[j][i].map(
                 (course) => {
                   const square = (
                     <div
-                      className="square"
+                      className="square small rounded-sm"
                       key={1}
-                      style={
-                      {
-                        height: `${((heightunit * course.length) / 30) + 6}px`,
-                        width: `${(14 * props.width) / 100}%`,
+                      style={{
+                        height: `${course.length}px`,
                         backgroundColor: course.color,
-                        color: 'black',
-                      }
-                    }
+                      }}
                     >
-                      {course.title}
+                      <div className="title px-1 text-black">
+                        <b>{course.title}</b>
+                      </div>
                     </div>
                   );
-                  if (props.link === true) {
+                  if (props.link) {
+                    // TODO: it has to be linked to the detailed course view.
                     return (
                       <a
                         href={'http://sugang.snu.ac.kr/sugang/cc/cc101.action?'
                       + 'openSchyy=2019&openShtmFg=U000200002&openDetaShtmFg='
                       + `U000300001&sbjtCd=${course.clanum}`
                       + `&ltNo=${course.lecnum}&sugangFlag=P`}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        key={0}
+                        style={{ height: `${course.length}px` }}
                       >
                         {square}
                       </a>
@@ -109,8 +111,17 @@ const TimetableView = (props) => {
   }
   return (
     <div className="TimetableView">
-      <table id="timetable" border="1" bordercolor="black" style={{ alignItem: 'center', width: '100%' }}>
+      <table id="timetable" border="1" bordercolor="black">
         <caption>{props.title}</caption>
+        <colgroup>
+          <col span="1" style={{ width: '10%' }} />
+          <col span="1" style={{ width: '15%' }} />
+          <col span="1" style={{ width: '15%' }} />
+          <col span="1" style={{ width: '15%' }} />
+          <col span="1" style={{ width: '15%' }} />
+          <col span="1" style={{ width: '15%' }} />
+          <col span="1" style={{ width: '15%' }} />
+        </colgroup>
         <tbody>
           {tablehtml}
         </tbody>
@@ -132,7 +143,6 @@ TimetableView.propTypes = {
     })),
   })).isRequired,
   height: PropTypes.number.isRequired,
-  width: PropTypes.number.isRequired,
   text: PropTypes.bool.isRequired,
   link: PropTypes.bool.isRequired,
   title: PropTypes.string.isRequired,
