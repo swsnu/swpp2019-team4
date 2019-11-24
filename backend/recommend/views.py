450dbc54ce3e02c9bb61cf0ff4fc49ceaebe12df
1207koo <1207koo@snu.ac.kr>
2019-11-24 22:50:56 +0900

    loop depth 1

--- backend/recommend/views.py
+++ backend/recommend/views.py
@@ -14,6 +14,9 @@ def collaborative_filtering(user):
     score_sum={}
     user_count={}
     score_average={}
+    user_sum={}
+    person_sum={}
+    user_person_sum={}
     relation={}
     score_weighted_sum={}
     relation_sum={}
@@ -25,43 +28,48 @@ def collaborative_filtering(user):
         user_count[course.id]=0.0
         score_weighted_sum[course.id]=0.0
         relation_sum[course.id]=0.0
+        score_result_dict[course.id]=-2.0
+    print('B')
+    for person in User.objects.all():
+        user_sum[person.id]=0.0
+        person_sum[person.id]=0.0
+        user_person_sum[person.id]=0.0
+    print('C')
     for score_data in CoursePref.objects.all():
         score_sum[score_data.course.id]+=score_data.score
         user_count[score_data.course.id]+=1.0
-    print('B')
+    print('D')
     for course in all_course:
         score_average[course.id]=(score_sum[course.id]-5.0*user_count[course.id])/(user_count[course.id]+division_error)+5.0
-    print('C')
+    print('E')
+    for score_data in CoursePref.objects.filter(user=user):
+        score_result_dict[score_data.course.id]=score_data.score
+    print('F')
+    for score_data in CoursePref.objects.all():
+        course=score_data.course
+        score=score_result_dict[course.id]
+        person=score_data.user
+        if score>-1:
+            user_delta=score-score_average[course.id]
+            person_delta=score_data.score-score_average[course.id]
+            user_sum[person.id]+=user_delta*user_delta
+            person_sum[person.id]+=person_delta*person_delta
+            user_person_sum[person.id]+=user_delta*person_delta
+    print('G')
     for person in User.objects.all():
-        user_sum=0.0
-        person_sum=0.0
-        user_person_sum=0.0
-        for user_score_data in CoursePref.objects.filter(user=user):
-            try:
-                person_score_data=CoursePref.objects.get(user=person, course=user_score_data.course)
-                course=user_score_data.course
-                user_delta=user_score_data.score-score_average[course.id]
-                person_delta=person_score_data.score-score_average[course.id]
-                user_sum=user_sum+user_delta*user_delta
-                person_sum=person_sum+person_delta*person_delta
-                user_person_sum=user_person_sum+user_delta*person_delta
-            except CoursePref.DoesNotExist:
-                continue
-        relation[person.id]=user_person_sum/(math.sqrt(user_sum*person_sum)+division_error)
-    print('D')
+        relation[person.id]=user_person_sum[person.id]/(math.sqrt(user_sum[person.id]*person_sum[person.id])+division_error)
+    print('H')
     for score_data in CoursePref.objects.all():
         relation_sum[score_data.course.id]+=relation[score_data.user.id]
         score_weighted_sum[score_data.course.id]+=score_data.score*relation[score_data.user.id]
-    print('E')
+    print('I')
     for course in all_course:
-        score_result_dict[course.id]=(score_weighted_sum[course.id]-5.0*relation_sum[course.id])/(relation_sum[course.id]+division_error)+5.0
-    print('F')
-    for score_data in CoursePref.objects.filter(user=user):
-        score_result_dict[score_data.course.id]=score_data.score
-    print('G')
+        if score_result_dict[course.id]<-1:
+            score_result_dict[course.id]=(score_weighted_sum[course.id]-5.0*relation_sum[course.id])/(relation_sum[course.id]+division_error)+5.0
+    print('J')
     for course in all_course:
         score_result.append({'course':course.id,'score':score_result_dict[course.id]})
-    print('H')
+    print('K')
     return score_result
 
 def api_course_pref(request):
