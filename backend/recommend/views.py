f81ddb7f43292e1cc7e06aea9b5472c6ba3bd11f
1207koo <1207koo@snu.ac.kr>
2019-11-29 17:31:58 +0900

    auth decorator added

--- backend/recommend/views.py
+++ backend/recommend/views.py
@@ -67,9 +67,9 @@ def collaborative_filtering(user):
         total_sum += score_data['score']
         total_count += 1.0
         total_square_sum += score_data['score']**2
-    
+
     if total_count > 0.0:
-        total_average = round(total_sum/total_count,3)
+        total_average = round(total_sum/total_count, 3)
         if total_count > 1.0:
             total_std = math.sqrt(total_square_sum/(total_count-1.0)-(total_average**2)*total_count/(total_count-1))
 
@@ -132,98 +132,101 @@ def collaborative_filtering(user):
 
     return score_result
 
+def auth_func(func):
+    def wrapper_function(*args, **kwargs):
+        if args[0].user.is_authenticated:
+            return func(*args, **kwargs)
+        return HttpResponse(status=401)
+    return wrapper_function
+
+@auth_func
 def api_course_pref(request):
-    if request.user.is_authenticated:
-        if request.method == 'GET':
-            return JsonResponse(collaborative_filtering(request.user), safe=False)
-        return HttpResponseNotAllowed(['GET'])
-    return HttpResponse(status=401)
+    if request.method == 'GET':
+        return JsonResponse(collaborative_filtering(request.user), safe=False)
+    return HttpResponseNotAllowed(['GET'])
 
+@auth_func
 def api_course_pref_id(request, course_id):
-    if request.user.is_authenticated:
-        if request.method == 'GET':
-            try:
-                course=Course.objects.get(id=course_id)
-                score_data = CoursePref.objects.get(user=request.user,course=course)
-            except (Course.DoesNotExist, CoursePref.DoesNotExist):
-                return JsonResponse({}, status=404, safe=False)
-            return JsonResponse(model_to_dict(score_data), safe=False)
-        if request.method == 'PUT':
-            try:
-                body = request.body.decode()
-                score = json.loads(body)['score']
-                if score<0 or score>10:
-                    return HttpResponseBadRequest()
-            except (KeyError, JSONDecodeError):
+    if request.method == 'GET':
+        try:
+            course = Course.objects.get(id=course_id)
+            score_data = CoursePref.objects.get(user=request.user, course=course)
+        except (Course.DoesNotExist, CoursePref.DoesNotExist):
+            return JsonResponse({}, status=404, safe=False)
+        return JsonResponse(model_to_dict(score_data), safe=False)
+    if request.method == 'PUT':
+        try:
+            body = request.body.decode()
+            score = json.loads(body)['score']
+            if score < 0 or score > 10:
                 return HttpResponseBadRequest()
-            try:
-                course=Course.objects.get(id=course_id)
-            except Course.DoesNotExist:
-                return JsonResponse({}, status=404, safe=False)
-            try:
-                score_data=CoursePref.objects.get(user=request.user,course=course)
-            except CoursePref.DoesNotExist:
-                new_score=CoursePref(user=request.user,course=course,score=score)
-                new_score.save()
-                return JsonResponse(model_to_dict(new_score),safe=False,status=201)
-            score_data.score=score
-            score_data.save()
-            return JsonResponse(model_to_dict(score_data),safe=False,status=200)
-        if request.method == 'DELETE':
-            try:
-                course=Course.objects.get(id=course_id)
-            except Course.DoesNotExist:
-                return HttpResponseNotFound()
-            try:
-                score_data=CoursePref.objects.get(user=request.user,course=course)
-            except CoursePref.DoesNotExist:
-                return HttpResponseNotFound()
-            score_data.delete()
-            return HttpResponse(status=200)
-        return HttpResponseNotAllowed(['GET', 'PUT', 'DELETE'])
-    return HttpResponse(status=401)
+        except (KeyError, JSONDecodeError):
+            return HttpResponseBadRequest()
+        try:
+            course = Course.objects.get(id=course_id)
+        except Course.DoesNotExist:
+            return JsonResponse({}, status=404, safe=False)
+        try:
+            score_data = CoursePref.objects.get(user=request.user, course=course)
+        except CoursePref.DoesNotExist:
+            new_score = CoursePref(user=request.user, course=course, score=score)
+            new_score.save()
+            return JsonResponse(model_to_dict(new_score), safe=False, status=201)
+        score_data.score = score
+        score_data.save()
+        return JsonResponse(model_to_dict(score_data), safe=False, status=200)
+    if request.method == 'DELETE':
+        try:
+            course = Course.objects.get(id=course_id)
+        except Course.DoesNotExist:
+            return HttpResponseNotFound()
+        try:
+            score_data = CoursePref.objects.get(user=request.user, course=course)
+        except CoursePref.DoesNotExist:
+            return HttpResponseNotFound()
+        score_data.delete()
+        return HttpResponse(status=200)
+    return HttpResponseNotAllowed(['GET', 'PUT', 'DELETE'])
 
+@auth_func
 def api_time_pref(request):
-    if request.user.is_authenticated:
-        if request.method == 'GET':
-            time_data=TimePref.objects.filter(user=request.user).values()
-            return JsonResponse(time_data, safe=False)
-        if request.method == 'PUT':
-            try:
-                body = request.body.decode()
-                score = json.loads(body)['score']
-                start_time = json.loads(body)['start_time']
-                weekday = json.loads(body)['week_day']
-                if score<0 or score>10:
-                    return HttpResponseBadRequest()
-            except (KeyError, JSONDecodeError):
+    if request.method == 'GET':
+        time_data = TimePref.objects.filter(user=request.user).values()
+        return JsonResponse(time_data, safe=False)
+    if request.method == 'PUT':
+        try:
+            body = request.body.decode()
+            score = json.loads(body)['score']
+            start_time = json.loads(body)['start_time']
+            weekday = json.loads(body)['week_day']
+            if score < 0 or score > 10:
                 return HttpResponseBadRequest()
-            try:
-                time_data=CoursePref.objects.get(user=request.user,weekday=weekday,start_time=start_time)
-            except TimePref.DoesNotExist:
-                new_score=TimePref(user=request.user,score=score,weekday=weekday,start_time=start_time)
-                new_score.save()
-                return JsonResponse(model_to_dict(new_score),safe=False,status=201)
-            time_data.score=score
-            time_data.save()
-            return JsonResponse(model_to_dict(time_data),safe=False,status=200)
-        return HttpResponseNotAllowed(['GET', 'PUT'])
-    return HttpResponse(status=401)
+        except (KeyError, JSONDecodeError):
+            return HttpResponseBadRequest()
+        try:
+            time_data = CoursePref.objects.get(user=request.user, weekday=weekday, start_time=start_time)
+        except TimePref.DoesNotExist:
+            new_score = TimePref(user=request.user, score=score, weekday=weekday, start_time=start_time)
+            new_score.save()
+            return JsonResponse(model_to_dict(new_score), safe=False, status=201)
+        time_data.score = score
+        time_data.save()
+        return JsonResponse(model_to_dict(time_data), safe=False, status=200)
+    return HttpResponseNotAllowed(['GET', 'PUT'])
 
+@auth_func
 def api_time_pref_id(request, timepref_id):
-    if request.user.is_authenticated:
-        if request.method == 'GET':
-            try:
-                time_data = CoursePref.objects.get(id=timepref_id, user=request.user)
-            except (TimePref.DoesNotExist):
-                return JsonResponse({}, status=404, safe=False)
-            return JsonResponse(model_to_dict(time_data), safe=False)
-        if request.method == 'DELETE':
-            try:
-                time_data=TimePref.objects.get(user=request.user,id=timepref_id)
-            except TimePref.DoesNotExist:
-                return HttpResponseNotFound()
-            time_data.delete()
-            return HttpResponse(status=200)
-        return HttpResponseNotAllowed(['GET', 'DELETE'])
-    return HttpResponse(status=401)
+    if request.method == 'GET':
+        try:
+            time_data = CoursePref.objects.get(id=timepref_id, user=request.user)
+        except TimePref.DoesNotExist:
+            return JsonResponse({}, status=404, safe=False)
+        return JsonResponse(model_to_dict(time_data), safe=False)
+    if request.method == 'DELETE':
+        try:
+            time_data = TimePref.objects.get(user=request.user, id=timepref_id)
+        except TimePref.DoesNotExist:
+            return HttpResponseNotFound()
+        time_data.delete()
+        return HttpResponse(status=200)
+    return HttpResponseNotAllowed(['GET', 'DELETE'])
