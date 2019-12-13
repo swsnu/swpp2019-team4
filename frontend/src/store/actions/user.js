cbeb3dd0a6033ea41d1ee80242b92d3440c1037b
vionicbest <vionicbest@snu.ac.kr>
2019-12-14 00:42:41 +0900

    add reset

--- frontend/src/store/actions/user.js
+++ frontend/src/store/actions/user.js
@@ -199,7 +199,9 @@ export const getUnratedCourse = (start, end, searchValues) => (dispatch) => axio
   .then((res) => dispatch({ type: actionTypes.GET_UNRATED_COURSE, course_list: res.data }))
   .catch(() => {});
 
-export const setRatedCourse = (start, end, searchValues) => (dispatch) => axios.get(
+export const setRatedCourse = (start, end, searchValues) => (dispatch) => {
+  dispatch({ type: actionTypes.RESET_RECOMMEND_COURSES});
+  axios.get(
   `/api/recommend/coursepref/rated/?start=${start}&end=${end}&title=${searchValues.title}`
   + `&classification=${searchValues.classification}&department=${searchValues.department}`
   + `&degree_program=${searchValues.degree_program}&academic_year=${searchValues.academic_year}`
@@ -209,9 +211,11 @@ export const setRatedCourse = (start, end, searchValues) => (dispatch) => axios.
   + `&min_score=${searchValues.min_score}&max_score=${searchValues.max_score}`,
 )
   .then((res) => dispatch({ type: actionTypes.SET_RATED_COURSE, course_list: res.data }))
-  .catch(() => {});
+  .catch(() => {});}
 
-export const setUnratedCourse = (start, end, searchValues) => (dispatch) => axios.get(
+export const setUnratedCourse = (start, end, searchValues) => (dispatch) => {
+  dispatch({ type: actionTypes.RESET_RECOMMEND_COURSES});
+  axios.get(
   `/api/recommend/coursepref/unrated/?start=${start}&end=${end}&title=${searchValues.title}`
   + `&classification=${searchValues.classification}&department=${searchValues.department}`
   + `&degree_program=${searchValues.degree_program}&academic_year=${searchValues.academic_year}`
@@ -220,7 +224,7 @@ export const setUnratedCourse = (start, end, searchValues) => (dispatch) => axio
   + `&min_credit=${searchValues.min_credit}&max_credit=${searchValues.max_credit}`,
 )
   .then((res) => dispatch({ type: actionTypes.SET_UNRATED_COURSE, course_list: res.data }))
-  .catch(() => {});
+  .catch(() => {});}
 
 export const putCourseprefTemp = (id, score) => (dispatch) => dispatch(
   { type: actionTypes.PUT_COURSEPREF_TEMP, coursepref: { id, score } },
