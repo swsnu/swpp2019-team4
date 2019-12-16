eaad013211b712647367215fabf286a8bcb84c5e
cube-c <cubecube1000@gmail.com>
2019-12-16 14:56:53 +0900

    Fix pylint and add simple test

--- backend/recommend/recommend.py
+++ backend/recommend/recommend.py
@@ -21,9 +21,10 @@ INFINITY = 100000000
 class Buildings:
     def __init__(self):
         buildings = list(Building.objects.all().values('id', 'latitude', 'longitude'))
-        self.building_id_to_pos = {building['id']: {'lat': float(building['latitude']), 'lng': float(building['longitude'])} 
+        self.building_id_to_pos = {building['id']: {'lat': float(building['latitude']),
+                                                    'lng': float(building['longitude'])}
                                    for building in buildings}
-    
+
     def distance(self, building_id_1, building_id_2):
         pos_1 = self.building_id_to_pos[building_id_1]
         pos_2 = self.building_id_to_pos[building_id_2]
@@ -49,11 +50,14 @@ class Timeslice:
         if weekday >= NUM_OF_WEEKDAYS:
             return
 
-        start_index = (min(max(start_time.hour * 60 + start_time.minute, HOUR_MIN * 60), HOUR_MAX * 60 - 1) - HOUR_MIN * 60) // 30
-        end_index = (min(max(end_time.hour * 60 + end_time.minute, HOUR_MIN * 60), HOUR_MAX * 60 - 1) - HOUR_MIN * 60) // 30
-        self.timeslice += [{'weekday': weekday, 'index': index, 'building_id': building_id} 
+        start_index = (min(max(start_time.hour * 60 + start_time.minute, HOUR_MIN * 60),
+                           HOUR_MAX * 60 - 1) - HOUR_MIN * 60) // 30
+        end_index = (min(max(end_time.hour * 60 + end_time.minute, HOUR_MIN * 60),
+                         HOUR_MAX * 60 - 1) - HOUR_MIN * 60) // 30
+        self.timeslice += [{'weekday': weekday, 'index': index, 'building_id': building_id}
                            for index in list(range(start_index, end_index + 1))]
-        self.time += [weekday * NUM_OF_TIME_PREFS_PER_DAY + index for index in list(range(start_index, end_index + 1))]
+        self.time += [weekday * NUM_OF_TIME_PREFS_PER_DAY + index
+                      for index in list(range(start_index, end_index + 1))]
         self.weekday[weekday] = True
 
     def get_weekday(self):
@@ -67,28 +71,29 @@ class Timeslice:
             for time2 in other.timeslice:
                 if time1['weekday'] is time2['weekday']:
                     diff = abs(time1['index'] - time2['index'])
-                    if diff is 0:
-                        return True 
-                    if diff is 1 and self.buildings.distance(time1['building_id'], time2['building_id']) > .5:
+                    if diff == 0:
+                        return True
+                    if diff == 1 and self.buildings.distance(
+                            time1['building_id'], time2['building_id']) > .5:
                         return True
         return False
-    
+
     def evaluate(self, timeslice_to_timepref):
-        if len(self.timeslice) is 0:
+        if not self.timeslice:
             return 0
         timepref_sum = 0
         for time in self.timeslice:
             timepref_sum += timeslice_to_timepref[time['weekday']][time['index']]
         return timepref_sum / len(self.timeslice)
-        
+
 class Constraints:
     def __init__(self, user):
         self.credit = (user.credit_min, user.credit_max)
         self.major_credit = (user.major_min, user.major_max)
         self.days_per_week = user.days_per_week
-    
+
     def penalty(self, credit, major_credit, days_per_week):
-        return max([0, self.credit[0] - credit, credit - self.credit[1], 
+        return max([0, self.credit[0] - credit, credit - self.credit[1],
                     self.major_credit[0] - major_credit, major_credit - self.major_credit[1],
                     days_per_week - self.days_per_week])
 
@@ -101,44 +106,56 @@ class TabuSearch:
         course_id_to_iid = dict(zip(self.iid_to_course_id, range(len(self.iid_to_course_id))))
 
         # handle course-time relationship
-        courses = list(Course.objects.filter(id__in=self.iid_to_course_id).values('id', 'course_number', 'credit', 'classification'))
+        courses = list(Course.objects.filter(id__in=self.iid_to_course_id)
+                       .values('id', 'course_number', 'credit', 'classification'))
         course_times = list(CourseTime.objects.filter(course__id__in=self.iid_to_course_id)
-            .values('course_id', 'building_id', 'weekday', 'start_time', 'end_time'))
-        
+                            .values('course_id', 'building_id', 'weekday',
+                                    'start_time', 'end_time'))
+
         course_id_to_course_number = {course['id']: course['course_number'] for course in courses}
         course_id_to_credit = {course['id']: course['credit'] for course in courses}
         course_id_to_classification = {course['id']: course['classification'] != '교양'
                                        for course in courses}
         buildings = Buildings()
-        self.iid_to_course_number = [course_id_to_course_number[course_id] for course_id in self.iid_to_course_id]
-        self.iid_to_credit = [course_id_to_credit[course_id] for course_id in self.iid_to_course_id]
-        self.iid_to_classification = [course_id_to_classification[course_id] for course_id in self.iid_to_course_id]
+        self.iid_to_course_number = [course_id_to_course_number[course_id]
+                                     for course_id in self.iid_to_course_id]
+        self.iid_to_credit = [course_id_to_credit[course_id]
+                              for course_id in self.iid_to_course_id]
+        self.iid_to_classification = [course_id_to_classification[course_id]
+                                      for course_id in self.iid_to_course_id]
         self.iid_to_timeslice = [Timeslice(buildings) for _ in range(len(self.iid_to_course_id))]
         for course_time in course_times:
             iid = course_id_to_iid[course_time['course_id']]
             self.iid_to_timeslice[iid].append(course_time)
-        
+
         # handle timepref to calculate overall score for each courses
-        time_prefs = list(TimePref.objects.filter(user=user).values('weekday', 'start_time', 'score'))
-        timeslice_to_timepref = [[DEFAULT_TIME_PREF] * NUM_OF_TIME_PREFS_PER_DAY for _ in range(NUM_OF_WEEKDAYS)]
+        time_prefs = list(TimePref.objects.filter(user=user)
+                          .values('weekday', 'start_time', 'score'))
+        timeslice_to_timepref = [[DEFAULT_TIME_PREF] * NUM_OF_TIME_PREFS_PER_DAY
+                                 for _ in range(NUM_OF_WEEKDAYS)]
         for time_pref in time_prefs:
-            index = (time_pref['start_time'].hour * 60 + time_pref['start_time'].minute - HOUR_MIN * 60) // 30
+            index = (time_pref['start_time'].hour * 60 + time_pref['start_time'].minute
+                     - HOUR_MIN * 60) // 30
             timeslice_to_timepref[time_pref['weekday']][index] = time_pref['score']
         for iid in range(len(self.iid_to_course_id)):
             self.iid_to_score[iid] *= self.iid_to_timeslice[iid].evaluate(timeslice_to_timepref)
 
         # remove similar courses (course_number and timeslice are same... score can be different)
-        argsort = sorted(list(range(len(self.iid_to_timeslice))), key=lambda iid: 
-            (self.iid_to_course_number[iid], self.iid_to_timeslice[iid].get_time_list(), -self.iid_to_score[iid]))
+        argsort = sorted(list(range(len(self.iid_to_timeslice))), key=lambda iid:
+                         (self.iid_to_course_number[iid],
+                          self.iid_to_timeslice[iid].get_time_list(),
+                          -self.iid_to_score[iid]))
         iid_save = []
-        if len(argsort) is not 0:
+        if argsort:
             iid_save.append(argsort[0])
         for i in range(len(argsort) - 1):
             iid_1 = argsort[i]
             iid_2 = argsort[i + 1]
-            if self.iid_to_course_number[iid_1] != self.iid_to_course_number[iid_2] or self.iid_to_timeslice[iid_1].get_time_list() != self.iid_to_timeslice[iid_2].get_time_list():
+            if self.iid_to_course_number[iid_1] != self.iid_to_course_number[iid_2] or \
+                self.iid_to_timeslice[iid_1].get_time_list() != \
+                self.iid_to_timeslice[iid_2].get_time_list():
                 # remove courses that the time does not exists
-                if len(self.iid_to_timeslice[iid_2].get_time_list()) > 0:
+                if self.iid_to_timeslice[iid_2].get_time_list():
                     iid_save.append(iid_2)
         iid_save = sorted(iid_save, key=lambda iid: -self.iid_to_score[iid])
 
@@ -152,7 +169,8 @@ class TabuSearch:
 
         # precompute 2D overlapping array
         self.iid_length = len(self.iid_to_course_id)
-        self.iid_overlap = [[False] * len(self.iid_to_course_id) for _ in range(len(self.iid_to_course_id))]
+        self.iid_overlap = [[False] * len(self.iid_to_course_id)
+                            for _ in range(len(self.iid_to_course_id))]
         for iid_1 in range(self.iid_length):
             timeslice_1 = self.iid_to_timeslice[iid_1]
             for iid_2 in range(self.iid_length):
@@ -169,41 +187,44 @@ class TabuSearch:
         self.best_candidate_solution = []
         self.best_candidate_fitness = self.evaluate(self.best_candidate_solution)
         self.best_candidate_tabu = []
-    
+
     def loop(self):
-        candidate_solutions = [self.get_neighbor(self.best_candidate_solution) for _ in range(CANDIDATES)]
+        candidate_solutions = [self.get_neighbor(self.best_candidate_solution)
+                               for _ in range(CANDIDATES)]
         self.best_candidate_solution = []
         self.best_candidate_fitness = (-INFINITY, 0)
         self.best_candidate_tabu = []
         for candidate_solution in candidate_solutions:
             candidate_fitness = self.evaluate(candidate_solution)
-            if candidate_fitness > self.best_candidate_fitness and not self.find_tabu(candidate_solution):
+            if candidate_fitness > self.best_candidate_fitness and \
+                not self.find_tabu(candidate_solution):
                 self.best_candidate_fitness = candidate_fitness
                 self.best_candidate_solution = candidate_solution
-            
-        valid = self.best_candidate_fitness[0] is 0
+
+        valid = self.best_candidate_fitness[0] == 0
         exist = False
         for result in self.results:
             if result['solution'] == self.best_candidate_solution:
                 exist = True
                 break
-        
+
         self.insert_tabu(self.best_candidate_solution)
-        
+
         if valid and not exist:
-            self.results.append({'solution': self.best_candidate_solution, 'fitness': self.best_candidate_fitness})
+            self.results.append({'solution': self.best_candidate_solution,
+                                 'fitness': self.best_candidate_fitness})
             for i in reversed(range(len(self.results) - 1)):
                 if self.results[i]['fitness'] >= self.results[i+1]['fitness']:
                     break
                 self.results[i], self.results[i + 1] = self.results[i + 1], self.results[i]
             if MAX_RESULTS < len(self.results):
                 self.results.pop()
-    
+
     def find_tabu(self, solution):
         return solution in self.tabu_list
-    
+
     def insert_tabu(self, solution):
-        self.tabu_list.append(solution) 
+        self.tabu_list.append(solution)
         while len(self.tabu_list) > MAX_TABU_SIZE:
             self.tabu_list.pop(0)
 
@@ -222,9 +243,9 @@ class TabuSearch:
         for day in weekday:
             if day is True:
                 days_per_week += 1
-        
+
         # constraint penalty
-        penalty = self.constraints.penalty(credit,major_credit, days_per_week)
+        penalty = self.constraints.penalty(credit, major_credit, days_per_week)
 
         # overlap & same course penalty
         for iid_1 in solution:
@@ -237,30 +258,30 @@ class TabuSearch:
 
         # preference score
         score = 0
-        if len(solution) > 0:
+        if solution:
             score_weighted_sum = 0
-            weight_sum = 0 
+            weight_sum = 0
             for iid in solution:
                 score_weighted_sum += self.iid_to_score[iid] * self.iid_to_credit[iid]
                 weight_sum += self.iid_to_credit[iid]
             score = score_weighted_sum / weight_sum
 
         return (-penalty, score)
-    
+
     def get_neighbor(self, solution):
         # edge case
-        if self.iid_length is 0:
+        if self.iid_length == 0:
             return []
 
         increment = False
-        if len(solution) is 0:
+        if not solution:
             increment = True
         elif len(solution) is self.iid_length:
             increment = False
         else:
             prob = random.random()
             increment = prob > 0.5
-        
+
         # deep copy
         new_solution = [iid for iid in solution]
         if increment:
@@ -272,19 +293,20 @@ class TabuSearch:
             index = random.randrange(len(solution))
             new_solution.pop(index)
         return sorted(new_solution)
-    
+
     def get_results(self):
         # print(self.results)
         results_iid = [result['solution'] for result in self.results]
-        results_course_id = [[self.iid_to_course_id[iid] for iid in result_iid] for result_iid in results_iid]
+        results_course_id = [[self.iid_to_course_id[iid] for iid in result_iid]
+                             for result_iid in results_iid]
         return results_course_id
 
 def run_recommendation(user):
     # get fundamental constants
-    ts = TabuSearch(user)
+    tabu = TabuSearch(user)
     for _ in range(ITERATION):
-        ts.loop()
-    results = ts.get_results()
+        tabu.loop()
+    results = tabu.get_results()
 
     color_gradient = [
         '#FF0037',
@@ -320,133 +342,3 @@ def run_recommendation(user):
         converted_results.append(empty_timetable)
 
     return converted_results
-
-    '''
-    all_course_data = list(map(ConvertedCourseData, get_target_courses(user)))
-    all_course_data.sort(key=
-                         lambda x: (
-                             x.get_timeslice_set().get_time_list(),
-                             user_data.course_score(x)),
-                         reverse=True)
-
-    unique_course_data = []
-    valid_course_data = []
-
-    for course_data in all_course_data:
-        if not course_data.get_timeslice_set().get_list():
-            continue
-        valid_course_data.append((user_data.course_score(course_data), course_data))
-        if unique_course_data:
-            back = unique_course_data[-1][1]
-            if back.get_timeslice_set().equals(course_data.get_timeslice_set()):
-                continue
-        unique_course_data.append((user_data.course_score(course_data), course_data))
-
-    unique_course_data.sort(key=lambda x: x[0], reverse=True)
-    valid_course_data.sort(key=lambda x: x[0], reverse=True)
-
-    def termi1(course_list):
-        my_credit = 0
-        for course in course_list:
-            my_credit += course.get_credit()
-        return my_credit > MAX_CREDIT
-
-    def appnd1(course_list):
-        my_credit = 0
-        for course in course_list:
-            my_credit += course.get_credit()
-        return my_credit >= MIN_CREDIT and course_list
-
-    candidates = []
-    backtrack(termi1,
-              appnd1,
-              user_data,
-              candidates,
-              0,
-              [],
-              TimesliceSet([]),
-              unique_course_data,
-              0)
-
-    answer = []
-    for candidate_pair in candidates:
-        (_, candidate) = candidate_pair
-
-        using_courses = []
-
-        for course1 in valid_course_data:
-            flag = False
-            for course2 in candidate:
-                if course1[1].get_timeslice_set().equals(course2.get_timeslice_set()):
-                    flag = True
-                    break
-            if flag:
-                using_courses.append(course1)
-
-        def termi2(course_list):
-            my_credit = 0
-            my_major = 0
-            for course in course_list:
-                cur_credit = course.get_credit()
-                my_credit += cur_credit
-                my_major += cur_credit if course.is_major() else 0
-            return my_credit > MAX_CREDIT or my_major > MAX_MAJOR
-
-        def appnd2(course_list):
-            my_credit = 0
-            my_major = 0
-            for course in course_list:
-                cur_credit = course.get_credit()
-                my_credit += cur_credit
-                my_major += cur_credit if course.is_major() else 0
-            return my_credit >= MIN_CREDIT and my_major >= MIN_MAJOR and course_list
-
-        backtrack(termi2,
-                  appnd2,
-                  user,
-                  answer,
-                  0,
-                  [],
-                  TimesliceSet([]),
-                  using_courses,
-                  0)
-
-    color_gradient = [
-        '#FF0037',
-        '#EB1637',
-        '#D72C37',
-        '#C34237',
-        '#AF5837',
-        '#9B6E37',
-        '#878437',
-        '#739A37',
-        '#5FB037',
-        '#4BC637',
-        '#37DC37',
-    ]
-
-    course_map = [None] * MAX_COURSE_ID
-    for course in Course.objects.all():
-        course_map[course.data()['id']] = course
-
-    answer = sample(answer[:min(100, len(answer))], min(20, len(answer)))
-
-    converted_answer = []
-    for timetable in answer:
-        recommend_timetable = RecommendTimetable(user=user)
-        recommend_timetable.save()
-        for _, converted_course in enumerate(timetable[1]):
-            course_pref = user_data.get_course_pref(converted_course)
-            color = color_gradient[round(course_pref)]
-            recommend_course = RecommendCourse(timetable=recommend_timetable,
-                                               course=course_map[converted_course.get_id()],
-                                               color=color)
-            recommend_course.save()
-        converted_answer.append(recommend_timetable)
-
-    if not converted_answer:
-        empty_timetable = RecommendTimetable(user=user)
-        empty_timetable.save()
-        converted_answer.append(empty_timetable)
-    return converted_answer
-    '''
